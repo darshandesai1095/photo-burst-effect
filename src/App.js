2b85a2ad7a8a501ccbd3f9390f91f873b11ef0a0
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-07 03:06:52 +0100

    sandbox cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import images from './images.js'
-import { motion, useScroll, useTransform } from 'framer-motion'
 import ImageCard from './components/ImageCard/ImageCard';
-import Description from './components/Description/Description';
-import DescriptionStack from './components/DescriptionStack/DescriptionStack';
 
 
 const App = () => {
 
   const l = images.length
-
-  // const { scrollYProgress } = useScroll()
-  // const activeImage = useTransform(scrollYProgress, (value) => {
-  //   const fraction = 1/l // switch with props
-  //   const activeImageNo = Math.floor(value/fraction)
-  //   console.log(activeImageNo)
-  //   return activeImageNo
-  // })
 
   const imageStack = images.map( (img, i) => {
     return (
@@ -35,7 +23,6 @@ const App = () => {
       <div className="main-content">
 
         <div className='flex-left'>
-          
         </div>
 
         <div className='flex-center'>
@@ -43,10 +30,6 @@ const App = () => {
         </div>
 
         <div className='flex-right'>
-          <div></div>
-          <div></div>
-          <div className='flex-right__description'><DescriptionStack/></div>
-          {/* <DescriptionStack/> */}
         </div>
 
       </div>
--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -1,16 +1,31 @@
-import { useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import './ImageCard.css';
 import { motion, useScroll, useTransform } from 'framer-motion'
 
-const ImageCard = ({ src, i, l }) => {
+const ImageCard = ({ src, i, l  }) => {
 
     const { scrollYProgress } = useScroll()
+    const [currentIndex, setCurrentIndex] = useState(0)
+  
+    useEffect(() => {
+      const getCurrentIndex = () => {
+        const fraction = 1/l // switch with props
+        const activeImageNo = Math.floor(scrollYProgress.current/fraction)
+        setCurrentIndex(activeImageNo)
+      }
+  
+      window.addEventListener('scroll', getCurrentIndex)
+  
+      return () => {
+        window.removeEventListener('scroll', getCurrentIndex)
+      }
+    }, [scrollYProgress])
 
     // if scrollYProgress is current, initial opacity = 1
     // => if scrollYProgress is 0.9*previous, opacity begins to increase
 
     const scale   = useTransform(scrollYProgress, [0.99*((i-1)/l), i/l, (i+1)/l], [1-(i*1/l), 1, 3])
-    const opacity = useTransform(scrollYProgress, [0.99*((i-1)/l), i/l, (i+1)/l], [0.1, 1.3, 0]) // 0.1 should scale with l
+    const opacity = useTransform(scrollYProgress, [0.99*((i-1)/l), i/l, (i+1)/l], [0.1, 1.2, 0]) // 0.1 should scale with l
     const yOffset = useTransform(scrollYProgress, [0.99*((i-1)/l), i/l, (i+1)/l], [-i*l/4, 0, 0]) // -i*10 should scale with l
     const blur    = useTransform(scrollYProgress, [0.99*((i-1)/l), i/l, (i+1)/l], [0, 0, 0])
 
@@ -23,7 +38,6 @@ const ImageCard = ({ src, i, l }) => {
         <>
             <motion.img 
                 src={src}
-                className="image-card"
                 style={{
                     scale: scale,
                     opacity: opacity,
